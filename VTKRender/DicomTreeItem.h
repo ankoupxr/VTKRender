d9f0a1da19c21cc82f2436dc3853570f7f55297d
ankoupxr <43463848+ankoupxr@users.noreply.github.com>
2022-07-12 01:07:12 +0800

    Dir

--- VTKRender/DicomTreeItem.h
+++ VTKRender/DicomTreeItem.h
@@ -13,31 +13,33 @@
 #include <QVector>
 #include <QStandardItem>
 
+namespace VTKRender {
 
-class DicomTreeItem:public QStandardItem
-{
-public:
-	explicit DicomTreeItem(const QVector<QVariant> &data, DicomTreeItem *parent = 0);
-	~DicomTreeItem();
+	class DicomTreeItem :public QStandardItem
+	{
+	public:
+		explicit DicomTreeItem(const QVector<QVariant>& data, DicomTreeItem* parent = 0);
+		~DicomTreeItem();
 
-	DicomTreeItem *child(int number);
-	int childCount() const;
-	int columnCount() const;
-	QVariant data(int column) const;
-	bool insertChildren(int position, int count, int columns);
-	bool insertColumns(int position, int columns);
-	DicomTreeItem *parent();
-	bool removeChildren(int position, int count);
-	bool removeColumns(int position, int columns);
-	int childNumber() const;
-	bool setData(int column, const QVariant &value);
+		DicomTreeItem* child(int number);
+		int childCount() const;
+		int columnCount() const;
+		QVariant data(int column) const;
+		bool insertChildren(int position, int count, int columns);
+		bool insertColumns(int position, int columns);
+		DicomTreeItem* parent();
+		bool removeChildren(int position, int count);
+		bool removeColumns(int position, int columns);
+		int childNumber() const;
+		bool setData(int column, const QVariant& value);
 
-private:
-	QList<DicomTreeItem*> childItems;
-	DicomTreeItem *parentItem;
-public:
-	QVector<QVariant> itemData;
-};
+	private:
+		QList<DicomTreeItem*> childItems;
+		DicomTreeItem* parentItem;
+	public:
+		QVector<QVariant> itemData;
+	};
 
+}
 
 #endif // TREEITEM_H
